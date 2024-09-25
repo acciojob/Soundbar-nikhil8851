6be738b9f8378b6f800dd80e3ac0bf944f1cd51c
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-25 11:36:50 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function playSound(sound) {
+    const audio = new Audio(`sounds/${sound}.mp3`);
+    audio.play();
+}
+
+function stopSound() {
+    const audios = document.querySelectorAll('audio');
+    audios.forEach(audio => audio.pause());
+}
